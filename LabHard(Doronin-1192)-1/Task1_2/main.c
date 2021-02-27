654b40e9a824bb60c940239c9809b842c1592996
panLukaszGelmintowski <72591420+panLukaszGelmintowski@users.noreply.github.com>
2021-02-27 11:00:36 +0500

    optimized

--- LabHard(Doronin-1192)-1/Task1_2/main.c
+++ LabHard(Doronin-1192)-1/Task1_2/main.c
@@ -14,6 +14,8 @@ int main(void)
 	GPIOB->MODER&=~( GPIO_MODER_MODER12 | GPIO_MODER_MODER13 | GPIO_MODER_MODER14 | GPIO_MODER_MODER15);
 	GPIOB->ODR|=0x100;
 	half_period = 50000;
+	uint32_t on[] = {0x1, 0x2, 0x4, 0x80, 0x40, 0x20, 0x10, 0x80};
+	uint32_t off[] = {0x10000, 0x20000, 0x40000, 0x800000, 0x400000, 0x200000, 0x100000, 0x800000}; 
 	while(1)
 	{
 		n=((GPIOB->IDR)&0x3000)>>12;
@@ -23,130 +25,52 @@ int main(void)
 		{
 			if(swreg==1)
 			{
-				GPIOB->BSRR|=0x1;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x2;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x4;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x80;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x40;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x20;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x10;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x8;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x10000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x20000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x40000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x800000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x400000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x200000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x100000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x80000;
-				delay(half_period<<n);
+				for (uint32_t i = 0; i<9; i++)
+				{
+					GPIOB->BSRR|=on[i];
+					delay(half_period<<n);
+				}
+				for (uint32_t j =0; j<9; j++)
+				{
+					GPIOB->BSRR|=off[j];
+					delay(half_period<<n);
+				}			
 			}
 			else
 			{
-				GPIOB->BSRR|=0x1;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x10000;
-				GPIOB->BSRR|=0x2;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x20000;
-				GPIOB->BSRR|=0x4;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x40000;
-				GPIOB->BSRR|=0x80;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x800000;
-				GPIOB->BSRR|=0x40;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x400000;
-				GPIOB->BSRR|=0x20;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x200000;
-				GPIOB->BSRR|=0x10;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x100000;
-				GPIOB->BSRR|=0x8;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x80000;
+				for (uint32_t i = 0; i<9; i++)
+				{
+					GPIOB->BSRR|=on[i];
+					delay(half_period<<n);
+					GPIOB->BSRR|=off[i];
+				}
 			}
 		}
 		else
 		{
+			{
 			if(swreg==1)
 			{
-				GPIOB->BSRR|=0x8;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x10;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x20;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x40;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x80;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x4;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x20;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x1;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x80000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x100000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x200000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x400000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x800000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x40000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x20000;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x10000;
-				delay(half_period<<n);
+				for (uint32_t i = 8; i>0; i--)
+				{
+					GPIOB->BSRR|=on[i];
+					delay(half_period<<n);
+				}
+				for (uint32_t j =8; j>0; j--)
+				{
+					GPIOB->BSRR|=off[j];
+					delay(half_period<<n);
+				}			
 			}
 			else
 			{
-				GPIOB->BSRR|=0x8;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x80000;
-				GPIOB->BSRR|=0x10;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x100000;
-				GPIOB->BSRR|=0x20;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x200000;
-				GPIOB->BSRR|=0x40;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x400000;
-				GPIOB->BSRR|=0x80;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x800000;
-				GPIOB->BSRR|=0x4;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x40000;
-				GPIOB->BSRR|=0x2;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x20000;
-				GPIOB->BSRR|=0x1;
-				delay(half_period<<n);
-				GPIOB->BSRR|=0x10000;
+				for (uint32_t i = 8; i>0; i--)
+				{
+					GPIOB->BSRR|=on[i];
+					delay(half_period<<n);
+					GPIOB->BSRR|=off[i];
+				}
+			}
 			}
 		}
 	}
