24e5d6abbb86853655c7eac51e9e718f6a2bf21d
panLukaszGelmintowski <72591420+panLukaszGelmintowski@users.noreply.github.com>
2021-02-28 17:48:53 +0500

    update

--- LabHard(Doronin-1192)-1/Task1_3/main.c
+++ LabHard(Doronin-1192)-1/Task1_3/main.c
@@ -3,22 +3,20 @@
 
 int main(void)
 {
-	uint32_t half_period;
-	uint32_t n; 
+	RCC->AHBENR|=RCC_AHBENR_GPIOBEN;
+	uint32_t half_period; 
 	uint32_t sw1; uint32_t sw2; uint32_t sw3; uint32_t sw4;
 	uint32_t btn = 14;
 	sw4=((GPIOB->IDR)&0x1000)>>14; sw3=((GPIOB->IDR)&0x2000)>>14; sw2=((GPIOB->IDR)&0x4000)>>14; sw1=((GPIOB->IDR)&0x8000)>>14;
-	RCC->AHBENR|=RCC_AHBENR_GPIOBEN;
 	GPIOB->MODER|=GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0 | GPIO_MODER_MODER2_0 |
 								GPIO_MODER_MODER3_0 | GPIO_MODER_MODER4_0 | GPIO_MODER_MODER5_0 |
 								GPIO_MODER_MODER6_0 | GPIO_MODER_MODER7_0 | GPIO_MODER_MODER9_0;
-	GPIOB->OSPEEDR|=0X5555; //все порты на скорость в 10 мгц - 0b0101010101010101
 	GPIOB->MODER&=~( GPIO_MODER_MODER12 | GPIO_MODER_MODER13 | GPIO_MODER_MODER14 | GPIO_MODER_MODER15);
 	GPIOB->ODR|=0x200;
 	half_period = 50000;
 	while(1)
 	{
-		uint32_t bit[] = {sw1, 1, sw2, 0, 1, sw3, 0, sw4}
+		uint32_t bit[] = {sw1, 1, sw2, 0, 1, sw3, 0, sw4};
 		uint32_t num[8];
 		uint32_t dec = 0; uint32_t div; uint32_t j = 7;
 		for (uint32_t i = 0; i < 8; i++)
@@ -32,7 +30,7 @@ int main(void)
 			num[i] = div;
 			dec -= div;
 			dec = dec / btn;
-			if (dec < b)
+			if (dec < btn)
 			{
 				i++;
 				num[i] = dec;
@@ -43,11 +41,31 @@ int main(void)
 			}
 		}
 		j = 0;
-		for (uint32_t = 7; i >=0; i--)
+		uint32_t o;
+		for (uint32_t i = 7; i >= 0; i--)
 		{
-			j = num[i];
-			if (j !=0)
+			o = num[i];
+			if (j ==0)
+			{
+				j = num[i];
+				if (j != 0)
+				{
+					output(o);
+					delay(200000);
+					GPIOB->BSRR|=0x0;
+					delay(50000);
+				}
+			}
+			else
+			{
+				output(o);
+				delay(200000);
+				GPIOB->BSRR|=0x0;
+				delay(50000);
+			}
 		}
+		//горит тчк
+		delay(500000);
 	}
 }
 
@@ -56,5 +74,75 @@ void delay(uint32_t count)
 for (uint32_t i=0;i<count;i++);
 }
 
-void output(uint32_t mas[]
+void output(uint32_t count)
+{
+	if (count == 0)
+	{
+		//
+	}
+	if (count == 1)
+	{
+		//
+	}
+	if (count == 2)
+	{
+		//
+	}
+	if (count == 3)
+	{
+		//
+	}
+	if (count == 4)
+	{
+		//
+	}
+	if (count == 5)
+	{
+		//
+	}
+	if (count == 6)
+	{
+		//
+	}
+	if (count == 7)
+	{
+		//
+	}
+	if (count == 8)
+	{
+		//
+	}
+	if (count == 9)
+	{
+		//
+	}
+	if (count == 10)
+	{
+		//
+	}
+	if (count == 11)
+	{
+		//
+	}
+	if (count == 12)
+	{
+		//
+	}
+	if (count == 13)
+	{
+		//
+	}
+	if (count == 14)
+	{
+		//
+	}
+	if (count == 15)
+	{
+		//
+	}
+	if (count == 16)
+	{
+		//
+	}
+}
  
--- LabHard(Doronin-1192)-1/Task1_3/main.h
+++ LabHard(Doronin-1192)-1/Task1_3/main.h
@@ -1,3 +1,5 @@
 #include <stm32f0xx.h>                  // Device header
 
 void delay(uint32_t);
+
+void output(uint32_t t);
